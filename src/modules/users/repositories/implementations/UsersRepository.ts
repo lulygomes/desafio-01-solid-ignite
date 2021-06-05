bc784ac66101f9e65f9007860e9ca8620bfb7198
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-06-05 13:35:26 -0300

    fix model

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -20,22 +20,42 @@ class UsersRepository implements IUsersRepository {
 
   create({ name, email }: ICreateUserDTO): User {
     // Complete aqui
+    const user = { name, email, admin: false };
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
     // Complete aqui
+    const userFind = this.users.find((user) => user.id === id);
+
+    return userFind;
   }
 
   findByEmail(email: string): User | undefined {
     // Complete aqui
+    const userFind = this.users.find((user) => user.email === email);
+
+    return userFind;
   }
 
   turnAdmin(receivedUser: User): User {
     // Complete aqui
+    const userIndex = this.users.findIndex(
+      (user) => user.id === receivedUser.id
+    );
+
+    if (userIndex < 0) {
+      throw new Error("User not found");
+    }
+
+    this.users[userIndex] = { ...this.users[userIndex], admin: true };
+    return this.users[userIndex];
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
